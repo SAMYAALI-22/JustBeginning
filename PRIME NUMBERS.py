26522fad3e251f1c38709e7f5be7838c8721158c
SAMYA ALI <alisamya182@gmail.com>
2024-09-04 01:44:02 +0530

    Add files via upload

---
+++ PRIME NUMBERS.py
@@ -0,0 +1,13 @@
+num = int(input("ENTER NUMBER:"))
+
+flag = 1
+for i in range(2, num):
+    if(num % i) == 0:
+        print(num,"is not a prime number")
+        print(i,"times",num//i,"is",num)
+        flag = 0
+        break
+
+    if flag == 1:
+        print(num,"is a prime number")
+print()
